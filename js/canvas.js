1e8be5477b12b5ffcce2a5a8ed31ad1d5b048b6f
placbo <Avulvab13>
2019-08-24 00:28:08 +0200

    initial commit

--- js/canvas.js
+++ js/canvas.js
@@ -1,20 +1,21 @@
 const canvas = document.getElementById("canvas");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
-let c = canvas.getContext("2d");
+let c; //context
+
+let elements = [];
+let stopped = true;
+
 
 function generateRandomColorString() {
-    return 'rgb(' +
-        (Math.floor(Math.random() * 255)) +
+    return (Math.floor(Math.random() * 200) + 50) +
         ', ' +
-        (Math.floor(Math.random() * 255)) +
+        (Math.floor(Math.random() * 255) + 0) +
         ', ' +
-        (Math.floor(Math.random() * 255)) +
-        ')';
+        (Math.floor(Math.random() * 200) + 50);
 }
 
 function Circle(_x, _y) {
-
     let radius = Math.random() * 4;
     let x = _x;
     let old_x;
@@ -22,8 +23,9 @@ function Circle(_x, _y) {
     let old_y;
     let dx = (Math.random() * 2 - 1) * 6;
     let dy = (Math.random() * 2 - 1) * 6;
-    let color = '#ffffff';
-    let countera = 0;
+    let colorString = generateRandomColorString();
+    let color;
+    let internalCounter = 0;
 
     let draw = () => {
         //c.fillRect(old_x, old_y, radius, radius);
@@ -35,60 +37,54 @@ function Circle(_x, _y) {
     };
 
     this.update = () => {
-        countera++;
+        internalCounter++;
         old_x = x;
         old_y = y;
-        color = 'rgb(255,255,255,' + (100 - countera) * 0.01 + ')';
-        if (x < radius || x > innerWidth - radius) {
-            dx = -dx;
-            color = generateRandomColorString();
-        }
-        if (y < radius || y > innerHeight - radius) {
-            dy = -dy;
-            color = generateRandomColorString();
-        }
+        color = 'rgb(' + colorString + (100 - internalCounter/2) * 0.01 + ')';
+        // if (x < radius || x > innerWidth - radius) {
+        //     dx = -dx;
+        //     color = generateRandomColorString();
+        // }
+        // if (y < radius || y > innerHeight - radius) {
+        //     dy = -dy;
+        //     color = generateRandomColorString();
+        // }
         x += dx;
         y += dy;
         draw();
     };
 }
 
-let circles = [];
-stopp = false;
-
-function startAnimation(startx, starty) {
-    c = canvas.getContext("2d");
-    stopp = false;
-    circles = [];
-    for (let i = 0; i < 100; i++) {
-        circles.push(new Circle(startx, starty));
-    }
-    animate();
-}
-
 function animate() {
-    console.log("animate");
-    if (stopp) {
-        console.log("stopping");
+    if (stopped) {
+        c.clearRect(0, 0, innerWidth, innerHeight);
         return;
     }
     requestAnimationFrame(animate);
     c.fillStyle = 'rgb(0, 0, 0, 0.15)';
     c.fillRect(0, 0, innerWidth, innerHeight);
-    for (let i = 0; i < circles.length; i++) {
-        circles[i].update();
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].update();
     }
-
-
 }
 
-canvas.addEventListener('mousedown', function (event) {
-    stopp = true;
-    console.log(stopp);
-    const rect = canvas.getBoundingClientRect();
-    x = event.clientX - rect.left;
-    y = event.clientY - rect.top;
-    console.log("x: " + x + " y: " + y);
-    startAnimation(x, y);
+canvas.addEventListener('mouseup', function (event) {
+    if (stopped) {
+        stopped = false;
+        const rect = canvas.getBoundingClientRect();
+        let startX = event.clientX - rect.left;
+        let startY = event.clientY - rect.top;
+        console.log("x: " + startX + " y: " + startY);
+        c = canvas.getContext("2d");
+        elements = [];
+        for (let i = 0; i < 100; i++) {
+            elements.push(new Circle(startX, startY));
+        }
+        animate();
+        console.log("Started");
+    } else {
+        stopped = true;
+        console.log("Stopped");
+    }
 });
 
